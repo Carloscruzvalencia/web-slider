09a279560a8c71caf71384290d7e76c7919d081e
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-12-17 04:05:36 +0100

    slider movement improved & working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,55 @@
 // variable que controla el numero de sliders que an pasado [1 a 4 se reinicia]
-slideN = 1;
+slideN = 0;
 
-// intervalo de 6s que gestiona cuando tiene que pasar el siguiente slider
 setInterval(() => {
-    // condicional que desavilita las transisiones en las sliders para evitar que se vean al volber a su posision original
-    if (slideN <= 3) {
-        document.getElementById("sl" + slideN).style.transition = "0s";
-    }
-    // condicion que mueve el slider 2 a su posision original para asi no interrumpir el slider
-    if (slideN == 2) {
-        document.getElementById("sl4").style.transform = "translate(100vw,0vw)"
-        document.getElementById("sl4").style.transition = "0s";
-
-    }
-    // console.log(slideN)
-    // suma 1 a la variable de control
-    slideN++
-}, 5000);
-
-// intervalo de 5s que gestiona cuando tienen que volber a su posicion original los sliders
-setInterval(function () {
-    // condicional que gestiona los 3 sliders para que vuelvan a su posicion original
-    if (slideN <= 4) {
-        document.getElementById("sl" + slideN).style.transform = "translate(0vw,0vw)"
-        document.getElementById("sl" + slideN).style.transition = "1s";
-    }
-
-    // console.log(slideN)
-
-    // condicional que resetea la variable slideN a 1 
-    if (slideN == 6) {
-        slideN = 1
-        // bucle que resetea la posicion al instante de todos los sliders 
-        for (i = 1; i != 4; i++) {
-            document.getElementById("sl" + i).style.transform = "translate(100vw,0vw)"
-            document.getElementById("sl" + slideN).style.transition = "0s";
-            // despues de resetear la posicion a los demas sliders mueve la 4 a el final de su trayecto 
-            document.getElementById("sl4").style.transform = "translate(-100vw,0vw)"
+    document.getElementById("sl3").style.transform = "translate(100vw)"
+    document.getElementById("sl3").style.transition = "0s"
+    document.getElementById("sl4").style.transform = "translate(100vw)"
+    document.getElementById("sl4").style.transition = "0s"
+    if (slideN <=4){
+        document.getElementById("sl"+slideN).style.transform = "translate(-100vw)"
+        document.getElementById("sl"+slideN).style.transition = "1s"
+        slideN++
+        if (slideN >= 5){
+            slideN = 0;
         }
     }
-}, 4000);
+}, 7000);
 
 setInterval(() => {
-        document.getElementById("b"+slideN).style.transform ="translateY(-11vh) rotate(10deg)"
-        // console.log("done")
-}, 2500);
+    document.getElementById("sl"+slideN).style.transform = "translate(0vw)"
+    document.getElementById("sl"+slideN).style.transition = "1s"
+    console.log(slideN)
+    if (slideN == 4){
+        for (i = 0 ; i != 3;i++){
+            document.getElementById("sl"+i).style.transform = "translate(100vw)"
+            document.getElementById("sl"+i).style.transition = "0s"
+        }
+    }
+}, 7000);
+
+
+
+
+// setInterval(() => {
+//     if (slideN <= 5) {
+//         document.getElementById("b" + slideN).style.transform = "translateY(-11vh) rotate(10deg)"
+//     } else {
+//         for (i = 1; i != 6; i++) {
+//             document.getElementById("b" + i).style.transform = "translateY(-11vh) rotate(0deg)"
+//         }
+//     }
+// }, 2200);
+
+
+
+
+// btype = ["Large bag", "Sathel", "Vintage", "Shoulder bag"]
+// bmarcas = ["Dior", "Cartier", "Corf"]
+// bnames = ["My ABC dior", "Scala", "C de cartier", "C de cartier mini", "Dusky"]
+
+// document.addEventListener("DOMContentLoaded", () => {
+    
+    
+
+// });
