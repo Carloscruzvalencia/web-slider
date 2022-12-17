414229db359b5949ba8c8b440e5947c79b117edb
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-12-17 05:24:07 +0100

    add typeit to project & created the animation for the productitles in the menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,32 +6,32 @@ setInterval(() => {
     document.getElementById("sl3").style.transition = "0s"
     document.getElementById("sl4").style.transform = "translate(100vw)"
     document.getElementById("sl4").style.transition = "0s"
-    if (slideN <=4){
-        document.getElementById("sl"+slideN).style.transform = "translate(-100vw)"
-        document.getElementById("sl"+slideN).style.transition = "1s"
+    if (slideN <= 4) {
+        document.getElementById("sl" + slideN).style.transform = "translate(-100vw)"
+        document.getElementById("sl" + slideN).style.transition = "1s"
         slideN++
-        if (slideN >= 5){
+        if (slideN >= 5) {
             slideN = 0;
         }
     }
 }, 7000);
 
 setInterval(() => {
-    document.getElementById("sl"+slideN).style.transform = "translate(0vw)"
-    document.getElementById("sl"+slideN).style.transition = "1s"
+    document.getElementById("sl" + slideN).style.transform = "translate(0vw)"
+    document.getElementById("sl" + slideN).style.transition = "1s"
     console.log(slideN)
-    if (slideN == 4){
-        for (i = 0 ; i != 3;i++){
-            document.getElementById("sl"+i).style.transform = "translate(100vw)"
-            document.getElementById("sl"+i).style.transition = "0s"
+    if (slideN == 4) {
+        for (i = 0; i != 3; i++) {
+            document.getElementById("sl" + i).style.transform = "translate(100vw)"
+            document.getElementById("sl" + i).style.transition = "0s"
         }
     }
 }, 7000);
 
 setInterval(() => {
     document.getElementById("b" + slideN).style.transform = "translateY(-11vh) rotate(10deg)"
-    if (slideN ==4 ){
-        for (i = 0 ; i != 4;i++){
+    if (slideN == 4) {
+        for (i = 0; i != 4; i++) {
             document.getElementById("b" + i).style.transform = "translateY(-11vh) rotate(00deg)"
         }
     }
@@ -39,12 +39,68 @@ setInterval(() => {
 
 
 
-// btype = ["Large bag", "Sathel", "Vintage", "Shoulder bag"]
-// bmarcas = ["Dior", "Cartier", "Corf"]
-// bnames = ["My ABC dior", "Scala", "C de cartier", "C de cartier mini", "Dusky"]
+document.addEventListener("DOMContentLoaded", () => {
 
-// document.addEventListener("DOMContentLoaded", () => {
-    
-    
-
-// });
+    // btype = ["Large bag", "Sathel", "Vintage", "Shoulder bag"]
+    // bmarcas = ["Dior", "Cartier", "Corf"]
+    bnames = ["My ABC dior", "Scala", "C de cartier", "C de cartier mini", "Dusky"]
+    new TypeIt("#item_title", {
+        waitUntilVisible: false,
+        cursor: false,
+        loop: true
+    })
+        .type(bnames[0])
+        .exec(async () => {
+            await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    return resolve();
+                }, 5200);
+            });
+        })
+        .delete()
+        .exec(async () => {
+            await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    return resolve();
+                }, 1000);
+            });
+        })
+        .type(bnames[1])
+        .exec(async () => {
+            await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    return resolve();
+                }, 4500);
+            });
+        })
+        .delete()
+        .type(bnames[2])
+        .exec(async () => {
+            await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    return resolve();
+                }, 4500);
+            });
+        })
+        .delete()
+        .type(bnames[3])
+        .exec(async () => {
+            await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    return resolve();
+                }, 5000);
+            });
+        })
+        .delete()
+        .type(bnames[4])
+        .exec(async () => {
+            await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    return resolve();
+                }, 4500);
+            });
+        })
+        .delete()
+        .go()
+        .reset();
+});
